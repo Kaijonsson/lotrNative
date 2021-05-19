006a0e26fee74cd18b193012b4b5fbb7594041c4
Kaijonsson <axel.jonsson@hotmail.com>
2021-05-19 17:33:31 +0200

    infinity loop

--- components/characterScreen/CharacterInfo.js
+++ components/characterScreen/CharacterInfo.js
@@ -1,14 +1,29 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import StarWarsCharacter from "./characterInfo/StarWarsCharacter";
+import LotrCharacter from "./characterInfo/LotrCharacter";
 
 const CharacterInfo = (props) => {
-  return (
-    <View>
-      <Text></Text>
-    </View>
-  );
+  const userChoice = props.props.userChoice;
+  const characterObject =
+    props.props.characterData ?? props.props.searchedCharacter;
+
+  const characterProps = () => {};
+  if (userChoice === "lotr") {
+    return <LotrCharacter data={characterObject} />;
+  }
+
+  if (userChoice === "starwars") {
+    return <StarWarsCharacter data={characterObject} />;
+  }
+
+  return <View style={styles.characterInfo}>{characterProps()}</View>;
 };
 
 export default CharacterInfo;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  characterInfo: {
+    paddingBottom: 100,
+  },
+});
--- screens/CharacterScreen.js
+++ screens/CharacterScreen.js
@@ -1,18 +1,17 @@
 import React from "react";
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, View } from "react-native";
 import CharacterCard from "../components/characterScreen/CharacterCard";
+import CharacterInfo from "../components/characterScreen/CharacterInfo";
+
 import Footer from "../components/app/Footer";
 
 function CharacterScreen({ route }) {
   const userChoice = route.params.userChoice;
 
-  console.log("userChoice: ", route.params.userChoice);
-  console.log("characterObject: ", route.params.characterData);
-  console.log("searchcharacterObject: ", route.params.searchedCharacter);
-
   return (
     <View style={styles.sizingContainer}>
       <CharacterCard props={route.params} />
+      <CharacterInfo props={route.params} />
       <Footer />
     </View>
   );
